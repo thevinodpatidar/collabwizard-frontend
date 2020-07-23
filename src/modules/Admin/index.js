e8d5f6e290b9bb79fe4d7550f557168db14f1432
vinodpatidar123 <pvinod813@gmail.com>
2020-07-23 20:00:35 +0530

    side bar

--- src/modules/Admin/index.js
+++ src/modules/Admin/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
-// import LandingPage from '../../components/LandingPage';
+import LandingPage from '../../components/LandingPage';
 import LoginPage from './Auth/Login';
 import Signup from './Auth/Signup';
 import ForgotPassword from './Auth/ForgotPassword';
 import ProtectedRoute from '../../components/ProtectedRoute';   
 import Resources from './Dashboard/Resources';
 import Dashboard from './Dashboard/Dashboard';
-import RouteWithSubRoutes from '../../components/RouterWithSubRoutes';
 import Home from './Dashboard/Home';
 
 export default function AdminModule({routes}) { 
     const match = useRouteMatch();
     return (
         <Switch>
-            {/* <Route exact path={`${match.path}`} component={LandingPage} /> */}
+            <Route exact path={`${match.path}`} component={LandingPage} />
             <Route exact path={`${match.path}login`} component={LoginPage} />
             <Route exact path={`${match.path}signup`} component={Signup} />
             <Route exact path={`${match.path}forgotPassword`} component={ForgotPassword} />
