7f1917379b5d2b4f8c4b09758b5231a40d1ad585
vinodpatidar123 <pvinod813@gmail.com>
2020-08-18 21:59:25 +0530

    cookies

--- src/config/apiendpoint.js
+++ src/config/apiendpoint.js
@@ -1,4 +1,4 @@
 module.exports = {
     local : "http://localhost:4000/api",
-    development : "http://ec2-13-233-113-79.ap-south-1.compute.amazonaws.com:4000/api"
+    development : "http://api.schooltribe.co:4000/api"
 }
