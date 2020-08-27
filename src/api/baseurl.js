87cd57bb0b98a57bd17d5bfce3bdb659d31352dd
vinodpatidar123 <pvinod813@gmail.com>
2020-08-28 02:54:34 +0530

    live link

--- src/api/baseurl.js
+++ src/api/baseurl.js
@@ -1,3 +1,3 @@
-const devEnv = true;
+const devEnv = false;
 export const baseURL = devEnv ? `http://localhost:8000/api` : `https://api.schooltribe.co/api`;
 export const baseUploadURL = devEnv ? `http://localhost:8000/api/upload/file`  : 'https://api.schooltribe.co/api/upload/file';
